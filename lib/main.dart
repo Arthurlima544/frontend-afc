4d00c6f60dc6fad29a1a3c9c072ac77705dce187
Arthurlima544 <arthurlindaolima544@gmail.com>
2025-08-30 18:15:22 -0300

    feat: basic homeview

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,13 @@
-import 'package:flutter/material.dart';
+import 'package:awesome_financial_control/pages/home.dart';
+import 'package:shadcn_flutter/shadcn_flutter.dart'
+    show ShadcnApp, runApp, ColorSchemes, ThemeData;
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
+  runApp(
+    ShadcnApp(
+      title: 'My App',
+      home: HomePage(),
+      theme: ThemeData(colorScheme: ColorSchemes.darkViolet, radius: 0.5),
+    ),
+  );
 }
---
+++ lib/pages/home.dart
@@ -0,0 +1,43 @@
+import 'package:shadcn_flutter/shadcn_flutter.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final WebViewController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onProgress: (int progress) {},
+          onPageStarted: (String url) {},
+          onPageFinished: (String url) {},
+          onHttpError: (HttpResponseError error) {},
+          onWebResourceError: (WebResourceError error) {},
+          onNavigationRequest: (NavigationRequest request) {
+            if (request.url.startsWith('https://www.youtube.com/')) {
+              return NavigationDecision.prevent;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse('https://flutter.dev'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(child: WebViewWidget(controller: controller)),
+    );
+  }
+}
